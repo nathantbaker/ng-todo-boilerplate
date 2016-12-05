78c8b4cbd6e839f508285acca3b9a66dc617dc81
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-05 11:29:42 -0600

    user sent to all items view after adding task

--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -1,12 +1,13 @@
 "use strict";
 
-app.controller('ItemNewCtrl', function($scope, ItemStorage) {
+app.controller('ItemNewCtrl', function($scope, $window, ItemStorage) {
   $scope.newTask = {};
   $scope.addNewItem = function(){
     $scope.newTask.isCompleted = false;
     $scope.newTask.id = $scope.items.length;
     ItemStorage.postNewItem($scope.newTask);
-    $scope.newTask = {};
+    $scope.newTask = {}; // clear newTask Form
+    $window.location.href = '/#/items/list'; // send user to list view if they add new task
   };
   $scope.items = [
     {
